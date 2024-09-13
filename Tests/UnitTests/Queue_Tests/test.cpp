1d4a237d8807036659f49ed08c89fe271e5b26ab
Jonny251989 <mailbox.lazarev@yandex.ru>
2024-09-13 19:49:29 +0300

    add changes in cmakelists and tests.cpp

--- Tests/UnitTests/Queue_Tests/test.cpp
+++ Tests/UnitTests/Queue_Tests/test.cpp
@@ -1,15 +1,20 @@
 #include "test.h"
 
-void QueueTest::SetUp() {
+    void QueueTest::SetUp() {
+ // Создаем очередь перед каждым тестом
+    }
 
-}
+    void QueueTest::TearDown() {
 
-void QueueTest::TearDown() {
-
-}
+    }
 
 TEST_F(QueueTest, NewQueueIsEmpty) {
   EXPECT_EQ(queue.size(), 0);
+}
+
+TEST_F(QueueTest, NewQueueFront) {
+  std::optional<std::reference_wrapper<int>> task = queue.front();
+  EXPECT_EQ(task, std::nullopt);
 }
 
 TEST_F(QueueTest, NewQueuePop) {
@@ -19,19 +24,15 @@ TEST_F(QueueTest, NewQueuePop) {
 
 TEST_F(QueueTest, Push) {
   queue.push(7);
-  std::cout<<"size = "<<queue.size()<<"\n";
   EXPECT_EQ(queue.size(), 1);
 }
 
 TEST_F(QueueTest, Front) {
-  queue.push(5);
+	queue.push(5);
   std::optional<int> task = queue.front();
   EXPECT_EQ(task.value(), 5);
-}
+  EXPECT_EQ(queue.size(), 1);
 
-TEST_F(QueueTest, NewQueueFront) {
-  std::optional<std::reference_wrapper<int>> task = queue.front();
-  EXPECT_EQ(task, std::nullopt);
 }
 
 TEST_F(QueueTest, NewQueueIsEmptyAfterAddAndPop) {
@@ -42,6 +43,7 @@ TEST_F(QueueTest, NewQueueIsEmptyAfterAddAndPop) {
   for(size_t i = 0; i < size; i++){
     queue.pop();
   }
+
   EXPECT_EQ(queue.size(), 0);
 }
 
@@ -57,18 +59,15 @@ TEST_F(QueueTest, QueueSizeAferAddAndSomePop) {
   EXPECT_EQ(queue.size(), limit/2);
 }
 
-
 TEST_F(QueueTest, QueueEqualElementAferAdd) {
   const size_t size = 100;
   std::deque<int> queue_temp;
   for(size_t i = 0; i < size; i++){
     queue.push(i + 2*i);
     queue_temp.push_back(i + 2*i);
-  }
-  for(size_t i = 0; i < size; i++){
     std::optional<int> value = queue.front();
-    queue.pop();
     EXPECT_EQ(value.value(), queue_temp[i]);
+    queue.pop();
   }
 }
 
@@ -78,14 +77,12 @@ TEST_F(QueueTest, QueueEqualFrontElementAferAdd) {
   for(size_t i = 0; i < size; i++){
     queue.push(i + 2*i);
     queue_temp.push_back(i + 2*i);
-  }
-  for(size_t i = 0; i < size; i++){
-    std::optional<int> value = queue.front();
-    EXPECT_EQ(value.value(), queue_temp[0]);
+    EXPECT_EQ(queue.front().value(), queue_temp[0]);
   }
 }
 
 TEST_F(QueueTest, FinishTestQueue) {
+
   EXPECT_EQ(queue.size(), 0);
   const size_t size = 100;
   std::deque<int> queue_temp;
@@ -94,6 +91,7 @@ TEST_F(QueueTest, FinishTestQueue) {
     queue_temp.push_back(i + 2*i);
   }
   EXPECT_EQ(queue.size(), size);
+  
   for(size_t i = 0; i < size; i++){
     std::optional<int> value = queue.front();
     EXPECT_EQ(value.value(), queue_temp[0]);
@@ -102,11 +100,25 @@ TEST_F(QueueTest, FinishTestQueue) {
     queue.pop();
     EXPECT_EQ(queue.size(), size - i - 1);
   }
+
   queue_temp.clear();
   queue.push(1);
   queue.push(7);
   queue.push(11);
   EXPECT_EQ(queue.size(), 3);
+
 }
 
+/*
 
+1) Добавить Н элеметов и удалить Н элементов: посмотреть размер после этих операций
+
+2) Добавить, к примеру, M элементов и удалить N элементов и посмотреть размер- равен ли он M-N 
+
+3) Проверить, работает ли очередь по принципу FIFO с помощью дополнительного контейнера, к примеру, вектора
+
+4) Протестировать работу метода front(): добавить N элементов и при этом метод front() должен соответствовать всегда
+одному и тому же первому элементу
+
+
+*/
