93a830de01a6414e590f2cba08be7a4f1f7d3447
Jonny251989 <mailbox.lazarev@yandex.ru>
2024-09-18 18:53:29 +0300

    new structure of CMakeLists and add class Server

--- CursedWordsTGBot/main.cpp
+++ CursedWordsTGBot/main.cpp
@@ -1,32 +1,17 @@
 #include <stdio.h>
 #include <tgbot/tgbot.h>
 #include <iostream>
+#include <ostream>
+#include "server.h"
+#include <string>
+
 
 int main(int argc, char *argv[]) {
 
-    TgBot::Bot bot("ХХХХXX");
-    bot.getEvents().onCommand("start", [&bot](TgBot::Message::Ptr message) {
-        bot.getApi().sendMessage(message->chat->id, "Hi!");
-    });
-    bot.getEvents().onAnyMessage([&bot](TgBot::Message::Ptr message) {
-        printf("User wrote %s\n", message->text.c_str());
+   std::string token = "7229787403:AAH0DVCx0wUQ-G9lkXYoIllHL0DhmdawEZo";
+   Server server(token);
+   server.start();
 
-        if (StringTools::startsWith(message->text, "/start")) {
-            return;
-        }
-        bot.getApi().sendMessage(message->chat->id, "Your message is: " + message->text);
-    });
-    
-    try {
-        printf("Bot username: %s\n", bot.getApi().getMe()->username.c_str());
-        TgBot::TgLongPoll longPoll(bot);
-        while (true) {
-            printf("Long poll started\n");
-            longPoll.start();
-        }
-    } catch (TgBot::TgException& e) {
-        printf("error: %s\n", e.what());
-    }
+    return 0;
 }
-
 
