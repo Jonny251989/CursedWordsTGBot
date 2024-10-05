933ec475e24139cc52adc6110acd7750689d7488
Jonny251989 <mailbox.lazarev@yandex.ru>
2024-10-05 20:22:55 +0300

    add logger.hpp and cmakelists.txt, sever.hpp with logger

--- CursedWordsTGBot/main.cpp
+++ CursedWordsTGBot/main.cpp
@@ -10,9 +10,10 @@
 
 int main(int argc, char *argv[]) {
 
-   std::shared_ptr<Queue<Task>> ptr_queue = std::make_shared<Queue<Task>>();
-   Server server(argv[1], ptr_queue);
-   server.start();
+    std::string token = "7229787403:AAH0DVCx0wUQ-G9lkXYoIllHL0DhmdawEZo";
+    std::shared_ptr<Queue<Task>> ptr_queue = std::make_shared<Queue<Task>>();
+    Server server(token, ptr_queue);
+    server.start();
 
     return 0;
 }
