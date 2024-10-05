ae37db46fdf9acea51b5259736f074f29edbaaa0
Jonny251989 <mailbox.lazarev@yandex.ru>
2024-10-05 19:42:14 +0300

    adding to task.hpp and server.hpp

--- CursedWordsTGBot/main.cpp
+++ CursedWordsTGBot/main.cpp
@@ -10,10 +10,9 @@
 
 int main(int argc, char *argv[]) {
 
-   std::string token = "7229787403:AAH0DVCx0wUQ-G9lkXYoIllHL0DhmdawEZo";
    std::shared_ptr<Queue<Task>> ptr_queue = std::make_shared<Queue<Task>>();
-   Server server(token, ptr_queue);
-   server.start();
+   Server server(argv[1], ptr_queue);
+   //server.start();
 
     return 0;
 }
