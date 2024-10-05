20ad33b8808a9349708b380beff3c3b142f3cf48
Jonny251989 <mailbox.lazarev@yandex.ru>
2024-10-05 11:42:26 +0300

    add .gitignore

--- build/Modules/Server/CMakeFiles/server_bot.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "libserver_bot.a"
-  "libserver_bot.pdb"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/server_bot.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
