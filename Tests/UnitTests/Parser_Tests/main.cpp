e7835938f94d26dec4f83d782d1194e3d38873cb
Jonny251989 <mailbox.lazarev@yandex.ru>
2024-11-09 10:38:32 +0300

    change tests parser.cpp

--- Tests/UnitTests/Parser_Tests/main.cpp
+++ Tests/UnitTests/Parser_Tests/main.cpp
@@ -4,9 +4,9 @@
 
 int main(int argc, char **argv){
 
-  ::testing::InitGoogleTest(&argc, argv);
-  ::testing::InitGoogleMock(&argc, argv);
-  
-  return RUN_ALL_TESTS();
+    ::testing::InitGoogleTest(&argc, argv);
+    ::testing::InitGoogleMock(&argc, argv);
+
+    return RUN_ALL_TESTS();
 }
 
