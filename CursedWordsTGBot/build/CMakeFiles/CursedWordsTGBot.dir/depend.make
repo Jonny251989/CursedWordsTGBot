e51467abae8a86c9b404a8d8e255e0c7bbc973fa
Jonny251989 <mailbox.lazarev@yandex.ru>
2024-11-11 10:59:27 +0300

    correcty path

---
+++ CursedWordsTGBot/build/CMakeFiles/CursedWordsTGBot.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CursedWordsTGBot.
+# This may be replaced when dependencies are built.
--- build.sh
+++ build.sh
@@ -4,6 +4,11 @@ mkdir build
 cd build
 cmake ..
 make -j$(nproc)
-cp ./CursedWordsTGBot/CursedWordsTGBot* /home/ivan/git_projects/CursedWordsTGBot/bins/CursedWordsTGBot/
-cp ./Tests/UnitTests/Queue_Tests/queue_test* /home/ivan/git_projects/CursedWordsTGBot/bins/Tests/Queue_test/
-cp ./Tests/UnitTests/Parser_Tests/parser_tests* /home/ivan/git_projects/CursedWordsTGBot/bins/Tests/Parser_tests/
+cp -r ./CursedWordsTGBot* ../bins/CursedWordsTGBot/
+cp ./Tests/UnitTests/Queue_Tests/queue_test* ../bins/Tests/Queue_test/
+cp ./Tests/UnitTests/Parser_Tests/parser_tests* ../bins/Tests/Parser_tests/
+
+
+#cp ./CursedWordsTGBot/CursedWordsTGBot* /home/ivan/git_projects/CursedWordsTGBot/bins/CursedWordsTGBot/
+#cp ./Tests/UnitTests/Queue_Tests/queue_test* /home/ivan/git_projects/CursedWordsTGBot/bins/Tests/Queue_test/
+#cp ./Tests/UnitTests/Parser_Tests/parser_tests* /home/ivan/git_projects/CursedWordsTGBot/bins/Tests/Parser_tests/
