ac8021a17963e28a6a500275679caa1fc79bf873
Jonny251989 <mailbox.lazarev@yandex.ru>
2024-11-11 10:59:27 +0300

    correcty path

---
+++ bins/CursedWordsTGBot/CursedWordsTGBot/CMakeFiles/CursedWordsTGBot.dir/DependInfo.cmake
@@ -0,0 +1,22 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/ivan/git_projects/CursedWordsTGBot/CursedWordsTGBot/main.cpp" "CursedWordsTGBot/CMakeFiles/CursedWordsTGBot.dir/main.cpp.o" "gcc" "CursedWordsTGBot/CMakeFiles/CursedWordsTGBot.dir/main.cpp.o.d"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/ivan/git_projects/CursedWordsTGBot/build/Modules/Server/CMakeFiles/server_bot.dir/DependInfo.cmake"
+  "/home/ivan/git_projects/CursedWordsTGBot/build/Modules/Parser/CMakeFiles/parser.dir/DependInfo.cmake"
+  "/home/ivan/git_projects/CursedWordsTGBot/build/Modules/Logger/CMakeFiles/logger.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
