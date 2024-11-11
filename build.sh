1ced7e7f455b8d457a9c828c90bd947d2434b026
Jonny251989 <mailbox.lazarev@yandex.ru>
2024-11-11 19:47:44 +0300

    correctly build.sh

--- build.sh
+++ build.sh
@@ -1,33 +1,14 @@
 #!/bin/bash -e
 
-script_directory=$(dirname "$(readlink -f "$0")")
-full_path=$(readlink -f "$script_directory/build")
+project_directory=$(dirname "$(readlink -f "$0")")
 
-mkdir $full_path
-cd $full_path
+cd $project_directory
+
+mkdir -p build
+cd build
 cmake ..
 make -j$(nproc)
 
-path_from_bot=$(readlink -f "$full_path/CursedWordsTGBot/CursedWordsTGBot*")
-path_to_bot=$(readlink -f "$script_directory/bins/CursedWordsTGBot/")
-
-path_from_unit_tests=$(readlink -f "$full_path/Tests/UnitTests")
-
-path_from_queue_tests=$(readlink -f "$path_from_unit_tests/Queue_Tests/queue_tests*")
-path_to_queue_tests=$(readlink -f "$script_directory/bins/Tests/Queue_Tests/")
-
-path_from_parser_tests=$(readlink -f "$path_from_unit_tests/Parser_Tests/parser_tests*")
-path_to_parser_tests=$(readlink -f "$script_directory/bins/Tests/Parser_Tests/")
-
-cp $path_from_bot $path_to_bot
-echo "script_directory: $script_directory"
-echo "path_from_bot: $path_from_bot"
-echo "path_to_bot: $path_to_bot"
-
-cp $path_from_queue_tests $path_to_queue_tests
-echo "path_from_queue_test: $path_from_queue_test"
-echo "path_to_queue_tests: $path_to_queue_tests"
-
-cp $path_from_parser_tests $path_to_parser_tests
-echo "path_from_parser_test: $path_from_parser_tests"
-echo "path_to_parser_test: $path_to_parser_tests"
+cp -p ./CursedWordsTGBot/CursedWordsTGBot* ../bins/CursedWordsTGBot/
+cp -p ./Tests/UnitTests/Queue_Tests/queue_tests* ../bins/Tests/Queue_Tests/
+cp -p ./Tests/UnitTests/Parser_Tests/parser_tests* ../bins/Tests/Parser_Tests/
