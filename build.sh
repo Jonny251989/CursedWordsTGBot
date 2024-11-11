f42981dfc6ba92c64404f9a7b8c9bba22c7fcee3
Jonny251989 <mailbox.lazarev@yandex.ru>
2024-11-11 11:00:37 +0300

    correcty path

--- build.sh
+++ build.sh
@@ -7,8 +7,3 @@ make -j$(nproc)
 cp -r ./CursedWordsTGBot* ../bins/CursedWordsTGBot/
 cp ./Tests/UnitTests/Queue_Tests/queue_test* ../bins/Tests/Queue_test/
 cp ./Tests/UnitTests/Parser_Tests/parser_tests* ../bins/Tests/Parser_tests/
-
-
-#cp ./CursedWordsTGBot/CursedWordsTGBot* /home/ivan/git_projects/CursedWordsTGBot/bins/CursedWordsTGBot/
-#cp ./Tests/UnitTests/Queue_Tests/queue_test* /home/ivan/git_projects/CursedWordsTGBot/bins/Tests/Queue_test/
-#cp ./Tests/UnitTests/Parser_Tests/parser_tests* /home/ivan/git_projects/CursedWordsTGBot/bins/Tests/Parser_tests/
