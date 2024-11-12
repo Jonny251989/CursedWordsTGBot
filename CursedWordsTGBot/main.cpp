8d9417ba218ce61d86de0a6e977502f7707a6cdb
Jonny251989 <mailbox.lazarev@yandex.ru>
2024-11-12 19:58:03 +0300

    small adding to build.sh and correct work with command line

--- CursedWordsTGBot/main.cpp
+++ CursedWordsTGBot/main.cpp
@@ -9,12 +9,27 @@
 #include "server.hpp"
 #include "parser.hpp"
 
+
+std::string get_arguments_string(size_t argc, char**argv){
+    std::string arguments_command_line = "";
+    for(size_t i = 1; i < argc; i++){
+        if(i != argc - 1)
+            arguments_command_line = arguments_command_line + argv[i] + " ";
+        else
+            arguments_command_line = arguments_command_line + argv[i];       
+    }
+    return arguments_command_line;
+}
+
 int main(int argc, char *argv[]) {
 
+    std::string arguments_command_line = get_arguments_string(argc, argv);
+    std::cout << "arguments:"<<arguments_command_line<<"\n";
+    
     Parser parser{{"-token"}};
     std::unordered_map<std::string, std::string> tokens;
     try{
-        tokens = parser.parse_string(argv[1]);
+        tokens = parser.parse_string(arguments_command_line);
     }
     catch(std::invalid_argument const& ex){
         Logger::getInstance().logInfo(Logger::Levels::Info, ex.what());
