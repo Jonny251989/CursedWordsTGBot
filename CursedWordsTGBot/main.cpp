21532f428c693cc61c4307d10d60dec36adc94de
Jonny251989 <mailbox.lazarev@yandex.ru>
2024-11-17 21:22:15 +0300

    all files changes to fit with last branch

--- CursedWordsTGBot/main.cpp
+++ CursedWordsTGBot/main.cpp
@@ -1,14 +1,21 @@
 #include <stdio.h>
+#include <csignal>
 #include <tgbot/tgbot.h>
 #include <iostream>
-#include <ostream>
-#include "queue.hpp"
-#include "task.hpp"
-#include "memory"
+#include <memory>
+#include <vector>
 #include <string>
-#include "server.hpp"
-#include "parser.hpp"
+#include <algorithm>
+#include <exception>
 #include <boost/algorithm/string.hpp>
+
+#include "logger.hpp"
+#include "task.hpp"
+#include "queue.hpp"
+#include "parser.hpp"
+#include "server.hpp"
+#include "worker.hpp"
+#include "signalhandler.hpp"
 
 std::string get_arguments_string(size_t argc, char**argv){
     std::vector<std::string> arguments_command_line;
@@ -20,26 +27,46 @@ std::string get_arguments_string(size_t argc, char**argv){
 
 int main(int argc, char *argv[]) {
 
-    std::string token = get_arguments_string(argc, argv);
-    Parser parser{{"-token"}};
-    std::unordered_map<std::string, std::string> tokens;
     try{
+        std::string token = get_arguments_string(argc, argv);
+        Parser parser{{"-token"}};
+        std::unordered_map<std::string, std::string> tokens;
         tokens = parser.parse_string(token);
-    }
-    catch(std::invalid_argument const& ex){
+        std::unique_ptr<TgBot::Bot> ptr_bot = std::make_unique<TgBot::Bot>(tokens["-token"]);
+
+        Logger::getInstance().setName(ptr_bot->getApi().getMe()->username);
+        Logger::getInstance().setLevel(Logger::Levels::Debug);
+
+        std::shared_ptr<Queue<ITask>> ptr_queue = std::make_shared<Queue<ITask>> ();
+        Server server(std::move(ptr_bot), ptr_queue);
+        Worker worker(ptr_queue);
+
+        SignalHandler handler({ SIGINT, SIGTERM }, [&](){
+                static int count = 0;
+                if (!count++) {
+                    server.terminate();
+                    worker.terminate();
+                    Logger::getInstance().logInfo(Logger::Levels::Critical, "Recieved second shutdown signal. Exiting!"); 
+                } else {
+                    Logger::getInstance().logInfo(Logger::Levels::Fatal, "Recieved shutdown signal. Stop polling!");
+                    std::exit(EXIT_FAILURE);
+                }
+            }
+        );
+        std::thread worker_thread(&Worker::run, &worker);
+        server.start();
+        worker_thread.join();
+
+    }catch(std::invalid_argument const& ex){
+        Logger::getInstance().logInfo(Logger::Levels::Info, ex.what());
+        std::exit(EXIT_FAILURE);
+    }catch(std::runtime_error const& ex){
+        Logger::getInstance().logInfo(Logger::Levels::Info, ex.what());
+        std::exit(EXIT_FAILURE);
+    }catch(std::exception const& ex){
         Logger::getInstance().logInfo(Logger::Levels::Info, ex.what());
         std::exit(EXIT_FAILURE);
     }
-    std::shared_ptr<Queue<Task>> ptr_queue = std::make_shared<Queue<Task>>();
-    std::unique_ptr<TgBot::Bot> ptr_bot= std::make_unique<TgBot::Bot>(tokens["-token"]);
-
-    Logger::getInstance().setName(ptr_bot->getApi().getMe()->username);
-    Logger::getInstance().setLevel(Logger::Levels::Debug);
-
-    Server server(std::move(ptr_bot), ptr_queue);
-
-    server.start();
 
     return 0;
-}
-
+};
---
+++ Modules/Worker/worker.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#include <iostream>
+#include <string>
+#include <memory>
+#include "task.hpp"
+#include "queue.hpp"
+
+
+class Worker{
+    public:
+
+    Worker(std::shared_ptr<Queue<ITask>> queue_ptr);
+    void run();
+    void terminate();
+    ~Worker();
+
+    private:
+    std::shared_ptr<Queue<ITask>> queue_ptr_;
+    bool shutdown_requested = false;
+};
