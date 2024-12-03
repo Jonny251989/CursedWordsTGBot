f2132af51afe1cc9f34438f6d6a6b4ab5acd397c
Jonny251989 <mailbox.lazarev@yandex.ru>
2024-12-03 22:01:10 +0300

    small changes of ./build-docker.sh

--- build-docker.sh
+++ build-docker.sh
@@ -1,5 +1,4 @@
 #!/bin/bash -e
 
 docker run --rm -it -v $(pwd):/app -w /app tgbot_base:1.0 /bin/bash -c "./build-local.sh"
-ls 
 docker build -t "tgbot:1.0" --build-arg BASE_IMAGE_VERSION=1.0 -f target_images/Dockerfile.CursedWordsTgBot .
