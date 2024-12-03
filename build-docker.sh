7e1a23bc543158da3b609f95ec4be68a07b62dc8
Jonny251989 <mailbox.lazarev@yandex.ru>
2024-12-03 21:53:37 +0300

    changes in scripts and docker's files

--- build-docker.sh
+++ build-docker.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -e
 
-docker build -t "tgbot_base:1.0" -f common_images/Dockerfile.base .
-
+docker run --rm -it -v $(pwd):/app -w /app tgbot_base:1.0 /bin/bash -c "./build-local.sh"
+ls 
 docker build -t "tgbot:1.0" --build-arg BASE_IMAGE_VERSION=1.0 -f target_images/Dockerfile.CursedWordsTgBot .
-
-docker run -it -e command_line_arguments="-token XXXXX" tgbot:1.0 
