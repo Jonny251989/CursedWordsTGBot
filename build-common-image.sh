3c5e444d3535c5f9d0e042d45a57a825192367eb
Jonny251989 <mailbox.lazarev@yandex.ru>
2024-12-03 21:53:37 +0300

    changes in scripts and docker's files

---
+++ build-common-image.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -e
+
+docker build -t "tgbot_base:1.0" -f common_images/Dockerfile.base .
---
+++ run-docker.sh
@@ -0,0 +1,3 @@
+#!/bin/bash -e
+
+docker run -it -e command_line_arguments="-token 7229787403:AAH0DVCx0wUQ-G9lkXYoIllHL0DhmdawEZo" tgbot:1.0 
