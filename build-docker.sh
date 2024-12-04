f0505e03344f667b51c63a5276fcb335697a971c
Jonny251989 <mailbox.lazarev@yandex.ru>
2024-12-04 21:43:54 +0300

    adding common and target images tar and change name of directory of tgbot

--- build-docker.sh
+++ build-docker.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 
-docker run --rm -it -v $(pwd):/app -w /app tgbot_base:1.0 /bin/bash -c "./build-local.sh"
+docker run --rm -it -v $(pwd):/tgbot -w /tgbot tgbot_base:1.0 /bin/bash -c "./build-local.sh"
 docker build -t "tgbot:1.0" --build-arg BASE_IMAGE_VERSION=1.0 -f target_images/Dockerfile.CursedWordsTgBot .
