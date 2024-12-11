5d9f34f2dc42fb81156eb49d6f794a65b57c90bd
Jonny251989 <mailbox.lazarev@yandex.ru>
2024-12-11 15:54:34 +0300

    script of run-docker.sh small changes

--- run-docker.sh
+++ run-docker.sh
@@ -1,3 +1,5 @@
 #!/bin/bash -e
 
-docker run -it -e command_line_arguments="-token 7229787403:AAH0DVCx0wUQ-G9lkXYoIllHL0DhmdawEZo" tgbot:1.0 
+source ./set_versions.sh
+
+docker run -it -e command_line_arguments="-token 7229787403:AAH0DVCx0wUQ-G9lkXYoIllHL0DhmdawEZo" "tgbot:${TARGET_IMAGE_VERSION}"
