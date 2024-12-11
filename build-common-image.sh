38f1977c6e6873d9274bd2a118e90d332aa075cc
Jonny251989 <mailbox.lazarev@yandex.ru>
2024-12-11 21:59:13 +0300

    Apply rejected changes from patch

--- build-common-image.sh
+++ build-common-image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 source ./set_versions.sh
 
-docker build -t "tgbot_base:${BASE_IMAGE_VERSION}" -f common_images/Dockerfile.base .
+docker build -t "cwbot_base:${COMMON_IMAGES_VERSION}" -f common_images/Dockerfile.base .
