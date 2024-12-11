d90e5deb95d5ed43f95c56406c541db0eb0f5af8
Jonny251989 <mailbox.lazarev@yandex.ru>
2024-12-11 21:15:22 +0300

    some changes in build-common-images.sh

--- build-common-image.sh
+++ build-common-image.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -e
 source ./set_versions.sh
 
-docker build -t "tgbot:${TARGET_IMAGE_VERSION}" -f common_images/Dockerfile.base .
+docker build -t "tgbot_base:${BASE_IMAGE_VERSION}" -f common_images/Dockerfile.base .
