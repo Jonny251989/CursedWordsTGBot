98de3f954722da8aac23c424da0f8eab3aacaed5
Jonny251989 <mailbox.lazarev@yandex.ru>
2024-12-23 22:32:32 +0300

    change build-local.sh

--- build-local.sh
+++ build-local.sh
@@ -13,10 +13,7 @@ mkdir -p ../bins/CursedWordsTGBot
 mkdir -p ../bins/Tests/Queue_Tests
 mkdir -p ../bins/Tests/Parser_Tests
 mkdir -p ../bins/Tests/SignalHandler_Tests
-<<<<<<< HEAD:build.sh
 
-=======
->>>>>>> 9277406da8f27723b8e641a9fc008d71da780727:build-local.sh
 cp ./CursedWordsTGBot/CursedWordsTGBot* ../bins/CursedWordsTGBot/
 cp ./Tests/UnitTests/Queue_Tests/queue_tests* ../bins/Tests/Queue_Tests/
 cp ./Tests/UnitTests/Parser_Tests/parser_tests* ../bins/Tests/Parser_Tests/
