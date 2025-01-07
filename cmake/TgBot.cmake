6d9ab3e9065c70fa16d716a81270fbd31faad0df
Jonny251989 <mailbox.lazarev@yandex.ru>
2025-01-07 03:19:59 +0300

    adding some changes

--- cmake/TgBot.cmake
+++ cmake/TgBot.cmake
@@ -1,21 +1,21 @@
 # cmake/TgBot.cmake
 
 # Проверка, была ли передана переменная BASE_PATH
-if(NOT BASE_PATH)
-    message(STATUS "BASE_PATH is not defined. Using default value.")
-    set(BASE_PATH "/usr/local")  # Указываем путь по умолчанию /usr/local/include 
+if(NOT TGBOT_CPP_INSTALL_PFEFIX)
+    message(STATUS "TGBOT_CPP_INSTALL_PFEFIX is not defined. Using default value.")
+    set(TGBOT_CPP_INSTALL_PFEFIX "/usr/local")  # Указываем путь по умолчанию /usr/local/include 
     
 else()
-    message(STATUS "Using BASE_PATH: ${BASE_PATH}")
-    set(TgBot_DIR "${BASE_PATH}/lib/cmake/TgBot/")
+    message(STATUS "Using TGBOT_CPP_INSTALL_PFEFIX: ${TGBOT_CPP_INSTALL_PFEFIX}")
+    set(TgBot_DIR "${TGBOT_CPP_INSTALL_PFEFIX}/lib/cmake/TgBot/")
 
 endif()
 
-set(TgBot_include_DIR "${BASE_PATH}/include/")
-set(TgBot_LIBRARIES "${BASE_PATH}/lib")
+set(TgBot_include_DIR "${TGBOT_CPP_INSTALL_PFEFIX}/include/")
+set(TgBot_LIBRARIES "${TGBOT_CPP_INSTALL_PFEFIX}/lib")
 
 find_package(TgBot REQUIRED)
 
-set(TGBOT_BASE_PATH ${BASE_PATH} CACHE PATH "Base path for the TgBot project")
+set(TGBOT_BASE_PATH ${TGBOT_CPP_INSTALL_PFEFIX} CACHE PATH "Base path for the TgBot project")
 set(TgBot_INCLUDE_DIR ${TgBot_include_DIR} CACHE PATH "Include directory for TgBot")
 set(TgBot_LIB_DIR ${TgBot_LIBRARIES} CACHE PATH "Library directory for TgBot")
