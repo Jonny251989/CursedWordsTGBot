cf61246ab675767190717a344f9c9d5300832ce5
Jonny251989 <mailbox.lazarev@yandex.ru>
2025-01-08 23:07:30 +0300

    changes

--- build-local.sh
+++ build-local.sh
@@ -7,10 +7,10 @@ cd $project_directory
 mkdir -p build
 cd build
 # cmake ..
-: ${BASE_PATH:=""}
+: ${TGBOT_CPP_INSTALL_PFEFIX:=""}
 
 # Запуск cmake с использованием переменной BASE_PATH
-cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=./build -DBASE_PATH=$BASE_PATH
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=./build -DTGBOT_CPP_INSTALL_PFEFIX=$TGBOT_CPP_INSTALL_PFEFIX
 make -j$(nproc)
 
 mkdir -p ../bins/CursedWordsTGBot
--- cmake/TgBot.cmake
+++ cmake/TgBot.cmake
@@ -1,21 +1,21 @@
 # cmake/TgBot.cmake
 
-# Проверка, была ли передана переменная BASE_PATH
-if(NOT BASE_PATH)
+# Проверка, была ли передана переменная TGBOT_CPP_INSTALL_PFEFIX
+if(NOT TGBOT_CPP_INSTALL_PFEFIX)
     message(STATUS "BASE_PATH is not defined. Using default value.")
-    set(BASE_PATH "/usr/local")  # Указываем путь по умолчанию /usr/local/include 
+    set(TGBOT_CPP_INSTALL_PFEFIX "/usr/local")
     
 else()
-    message(STATUS "Using BASE_PATH: ${BASE_PATH}")
-    set(TgBot_DIR "${BASE_PATH}/lib/cmake/TgBot/")
+    message(STATUS "Using TGBOT_CPP_INSTALL_PFEFIX: ${TGBOT_CPP_INSTALL_PFEFIX}")
+    set(TgBot_DIR "${TGBOT_CPP_INSTALL_PFEFIX}/lib/cmake/TgBot/")
 
 endif()
 
-set(TgBot_include_DIR "${BASE_PATH}/include/")
-set(TgBot_LIBRARIES "${BASE_PATH}/lib")
 
-find_package(TgBot REQUIRED)
+set(TgBot_include_DIR "${TGBOT_CPP_INSTALL_PFEFIX}/include/")
+set(TgBot_LIBRARIES "${TGBOT_CPP_INSTALL_PFEFIX}/lib" )
 
-set(TGBOT_BASE_PATH ${BASE_PATH} CACHE PATH "Base path for the TgBot project")
+
+set(TGBOT_CPP_INSTALL_PFEFIX ${TGBOT_CPP_INSTALL_PFEFIX} CACHE PATH "TGBOT_CPP_INSTALL_PFEFIX for the TgBot project")
 set(TgBot_INCLUDE_DIR ${TgBot_include_DIR} CACHE PATH "Include directory for TgBot")
 set(TgBot_LIB_DIR ${TgBot_LIBRARIES} CACHE PATH "Library directory for TgBot")
