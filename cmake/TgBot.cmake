22afebb6b613c8a8ed6584344dc58ea32fca633a
Jonny251989 <mailbox.lazarev@yandex.ru>
2025-01-09 00:00:17 +0300

    some adding changes

--- cmake/TgBot.cmake
+++ cmake/TgBot.cmake
@@ -3,7 +3,7 @@
 # Проверка, была ли передана переменная BASE_PATH
 if(NOT TGBOT_CPP_INSTALL_PREFIX)
     message(STATUS "TGBOT_CPP_INSTALL_PREFIX is not defined. Using default value.")
-    set(TGBOT_CPP_INSTALL_PFEFIX "/usr/local")  # Указываем путь по умолчанию /usr/local/include 
+    set(TGBOT_CPP_INSTALL_PREFIX "/usr/local")  # Указываем путь по умолчанию /usr/local/include 
     
 else()
     message(STATUS "Using TGBOT_CPP_INSTALL_PREFIX: ${TGBOT_CPP_INSTALL_PREFIX}")
@@ -12,8 +12,8 @@ else()
 endif()
 
 set(TgBot_include_DIR "${TGBOT_CPP_INSTALL_PREFIX}/include/")
-set(TgBot_LIBRARIES "${TGBOT_CPP_INSTALL_PREFIX}/lib/libTgBot.a" ${CMAKE_THREAD_LIBS_INIT} ${OPENSSL_LIBRARIES} ${Boost_LIBRARIES} ${CURL_LIBRARIES})
+set(TgBot_LIBRARIES "${TGBOT_CPP_INSTALL_PREFIX}/lib")
 
-set(TGBOT_CPP_INSTALL_PREFIX ${TGBOT_CPP_INSTALL_PREFIX} CACHE PATH "TGBOT_CPP_INSTALL_PREFIX for the TgBot project")
+set(TGBOT_CPP_INSTALL_PREFIX_PATH ${TGBOT_CPP_INSTALL_PREFIX} CACHE PATH "TGBOT_CPP_INSTALL_PREFIX for the TgBot project")
 set(TgBot_INCLUDE_DIR ${TgBot_include_DIR} CACHE PATH "Include directory for TgBot")
 set(TgBot_LIB_DIR ${TgBot_LIBRARIES} CACHE PATH "Library directory for TgBot")
