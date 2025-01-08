022d4d2c700e3e6affe01c3b9e2fd005e8ee70e6
Jonny251989 <mailbox.lazarev@yandex.ru>
2025-01-08 23:38:27 +0300

    some adding changes

--- cmake/TgBot.cmake
+++ cmake/TgBot.cmake
@@ -14,6 +14,7 @@ endif()
 set(TgBot_include_DIR "${TGBOT_CPP_INSTALL_PFEFIX}/include/")
 set(TgBot_LIBRARIES "${TGBOT_CPP_INSTALL_PFEFIX}/lib/libTgBot.a" ${CMAKE_THREAD_LIBS_INIT} ${OPENSSL_LIBRARIES} ${Boost_LIBRARIES} ${CURL_LIBRARIES})
 
+find_package(TgBot)
 
 set(TGBOT_CPP_INSTALL_PFEFIX ${TGBOT_CPP_INSTALL_PFEFIX} CACHE PATH "TGBOT_CPP_INSTALL_PFEFIX for the TgBot project")
 set(TgBot_INCLUDE_DIR ${TgBot_include_DIR} CACHE PATH "Include directory for TgBot")
