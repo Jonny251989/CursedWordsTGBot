07363a2617526859b1b48010a286cdf0d9337712
Jonny251989 <mailbox.lazarev@yandex.ru>
2025-01-15 17:49:44 +0300

    some changes sending signal to CWBot and messageID

--- Tests/FunctionalTests/Reactor_Tests/test_reactorsresult.cpp
+++ Tests/FunctionalTests/Reactor_Tests/test_reactorsresult.cpp
@@ -16,40 +16,46 @@ void ReactorResultTest::SetUp() {
 void ReactorResultTest::generator(){
     chat_id = -1002432345513;
     std::ifstream inputFile("./messages.txt");
+    //std::ifstream inputFile("./Tests/FunctionalTests/Reactor_Tests/messages.txt");
     if (!inputFile) {
         std::cerr << "Не удалось открыть файл!" << std::endl;
     }
     std::string line;
     while (std::getline(inputFile, line)) {  
-        t_bot->getApi().sendMessage(chat_id, line);
-        gen_message = line;
-        m_map[gen_message] = false;
+        auto message = t_bot->getApi().sendMessage(chat_id, line);
+        send_messageId = message->messageId;
+        std::cout<<"SEND MESSAGE WITH ID "<< message->messageId<<"\n";
+        m_map[send_messageId];
         std::this_thread::sleep_for(std::chrono::seconds(3));
     }
     inputFile.close();
-    std::cout<<"Generator works!\n";
-    //kill(getpid(), SIGTERM);
+    std::cout<<"Generator's works finished!\n";
+}
+
+void* thread_function(void* arg) {
+    run_bot("7229787403:AAH0DVCx0wUQ-G9lkXYoIllHL0DhmdawEZo");
+    return nullptr;
 }
 
 TEST_F(ReactorResultTest, FirstTest) {
     auto generator_f = [&](){
         generator();
     };
-    // auto checker_f = [&](){
-    //     checker();
-    // };
-    auto main_f = [&](){
-        run_bot("7229787403:AAH0DVCx0wUQ-G9lkXYoIllHL0DhmdawEZo");
-    };
-    std::thread mainThread{main_f};
+
+    pthread_t thread;
+    pthread_create(&thread, nullptr, thread_function, nullptr);
+
     std::thread generatorThread{generator_f};     
     checker();
-    kill(getpid(), SIGINT);
+    pthread_kill(thread, SIGINT);
+
+    pthread_join(thread, nullptr);
+
     for(auto it = m_map.begin(); it != m_map.end(); it++){
-        std::cout<<"key: "<<it->first<<", value: "<<it->second<<"\n";
+        EXPECT_EQ(std::abs(it->first - it ->second), 1);
     }
     generatorThread.join();
-    mainThread.join();
+    pthread_join(thread, nullptr);
 }
 
 void ReactorResultTest::checker(){
@@ -58,20 +64,10 @@ void ReactorResultTest::checker(){
     std::chrono::duration<double> elapsed_seconds = std::chrono::duration<double>::zero();
 
     t_bot->getEvents().onAnyMessage([&](TgBot::Message::Ptr message) {
-        // message->messageId;
-        // message->replyToMessage->text;
-        if(message->replyToMessage !=nullptr)
-            std::cout<<"replyToMessage: "<<message->replyToMessage->text<<",\n";
-            
-        //elapsed_seconds = std::chrono::duration<double>::zero();
+        std::cout<<"GET ASNWER MESSAGE WITH ID "<< message->messageId<<"\n";
         last_change_time = std::chrono::steady_clock::now();
-        //now = std::chrono::steady_clock::now();
-        answ_message = message->text;
-        if ((answ_message.find(gen_message) != std::string::npos) && !m_map[gen_message]) {
-            m_map[gen_message] = true;
-        }
-
-        
+        answer_messageId = message->messageId;
+        m_map[send_messageId] = answer_messageId;
     });
     try {
         TgBot::TgLongPoll longPoll( *t_bot);
@@ -84,45 +80,3 @@ void ReactorResultTest::checker(){
         printf("error: %s\n", e.what());
     }
 }
-
-
-
-
-
-
-
-
-
-
-    //run_bot("7229787403:AAH0DVCx0wUQ-G9lkXYoIllHL0DhmdawEZo");
-    // generatorThread.join()
-
-
-    // auto checker_f = [&](){
-    //     checker();
-    // };
-
-    // auto main_f = [&](){
-    //     run_bot("7229787403:AAH0DVCx0wUQ-G9lkXYoIllHL0DhmdawEZo");
-    // };
-
-    //std::thread generatorThread{generator_f};
-
-    //checker(); 
-   // generator();
-
-
-    //generator();
-
-
-    //std::thread checkerThread{checker_f}; 
-
-    //generatorThread.join();   
-    //checker(); 
-    //checker();
-    //std::thread generatorThread{generator_f};
-    //generatorThread.join();
-;
-    //std::thread checkerThread{checker_f}; 
-    //checkerThread.join();
-    //checker();
