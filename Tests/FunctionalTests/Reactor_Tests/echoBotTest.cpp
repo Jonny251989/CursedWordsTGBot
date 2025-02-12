d097b263723fd436fe0be5e1dcb147f3e1d5c614
Jonny251989 <mailbox.lazarev@yandex.ru>
2025-02-12 17:07:29 +0300

    some changes else

---
+++ Tests/FunctionalTests/Reactor_Tests/echoBotTest.cpp
@@ -0,0 +1,64 @@
+#include "echoBotTest.hpp"
+
+void ReactorResultTest::TearDown() {
+
+}
+
+void ReactorResultTest::SetUp() {
+    std::string token = "7389966079:AAHXCquKT0JaQUqHRzac8MMsXMCUUd5uvXQ";
+    t_bot = std::make_shared<TgBot::Bot>(token);
+    count_recieve_messages = 0;
+    chat_id_ = -1002432345513;
+}
+
+void ReactorResultTest::generator(){
+    std::ifstream inputFile("./Tests/FunctionalTests/Reactor_Tests/messages.txt");
+    if (!inputFile) {
+        std::cerr << "Не удалось открыть файл!" << std::endl;
+    }
+    std::string line;
+    while (std::getline(inputFile, line)) {
+        {
+            std::lock_guard lg{set_mutex};
+            message_container.insert(line);
+        }
+        t_bot->getApi().sendMessage(chat_id_, line);
+    }
+    inputFile.close();
+}
+
+void ReactorResultTest::checker(){
+    auto last_change_time = std::chrono::steady_clock::now();
+    std::chrono::duration<double> elapsed_seconds = std::chrono::duration<double>::zero();
+
+    t_bot->getEvents().onAnyMessage([&](TgBot::Message::Ptr message) {
+            count_recieve_messages++;
+            std::lock_guard lg{set_mutex};
+            ASSERT_EQ(message_container.count(message->replyToMessage->text), true);
+            last_change_time = std::chrono::steady_clock::now(); 
+    });
+    try {
+        TgBot::TgLongPoll longPoll( *t_bot);
+        while (count_recieve_messages <= limit_sent_messages_ && elapsed_seconds.count() < limit_time_in_sec) {
+            longPoll.start();
+            elapsed_seconds = std::chrono::steady_clock::now() - last_change_time;
+        }
+    } catch (TgBot::TgException& e) {
+        printf("error: %s\n", e.what());
+    }
+}
+
+TEST_F(ReactorResultTest, FirstTest) {
+
+    std::jthread mainThread{[&](){
+        run_bot("7229787403:AAH0DVCx0wUQ-G9lkXYoIllHL0DhmdawEZo");
+    }};    
+
+    generator();
+    std::this_thread::sleep_for(std::chrono::seconds(3));
+
+    std::raise(SIGINT);
+    
+    checker();
+
+}
--- Tests/FunctionalTests/Reactor_Tests/echoBotTest.hpp
+++ Tests/FunctionalTests/Reactor_Tests/echoBotTest.hpp
@@ -20,6 +20,14 @@
 #include <csignal>
 
 
+struct Message{
+    std::int32_t  messageId_;
+    std::string messageText_;
+    Message( std::int32_t messageId,  std::string messageText): messageId_(messageId), messageText_(messageText) {
+
+    }
+};
+
 class ReactorResultTest : public ::testing::Test{
 protected:
     void SetUp() override;
@@ -28,10 +36,11 @@ protected:
     void checker();
 
 private:
-    size_t count_sending_messages;
+    std::atomic<size_t> count_recieve_messages;
     std::shared_ptr<TgBot::Bot> t_bot;
-    std::int64_t chat_id;
-    std::string sent_message;
-    const size_t size_map = 10;
-    const size_t limit_time = 6;
+    std::int64_t chat_id_;
+    const size_t limit_sent_messages_ = 5;
+    const size_t limit_time_in_sec = 10;
+    std::set<std::string> message_container;
+    std::mutex set_mutex;
 };
