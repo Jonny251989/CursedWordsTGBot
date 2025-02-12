a546a111ed91c83e7a6b85cc0a4c440126323f57
Jonny251989 <mailbox.lazarev@yandex.ru>
2025-02-12 17:20:13 +0300

    some changes else

--- Modules/Reactor/reactor.cpp
+++ Modules/Reactor/reactor.cpp
@@ -1,15 +1,14 @@
 #include "reactor.hpp"
 
-EchoReactor::EchoReactor(std::shared_ptr<TgBot::Bot> ptr_bot, std::string message, std::int64_t chat_id, std::int32_t messageId, TgBot::User::Ptr from): 
-ptr_bot_(ptr_bot), message_(message), chat_id_(chat_id), messageId_(messageId), from_(from){
+EchoReactor::EchoReactor(std::shared_ptr<TgBot::Bot> ptr_bot, std::string message, std::int64_t chat_id, std::int32_t messageId): 
+ptr_bot_(std::move(ptr_bot)), message_(std::move(message)), chat_id_(chat_id), messageId_(messageId){
 
 }
 
 void EchoReactor::react(bool classification_result){
 
-    ptr_bot_->getApi().sendMessage(chat_id_, "Message with ID " + std::to_string(messageId_) + " is cursed word!",
-     nullptr, std::make_shared<TgBot::ReplyParameters>(messageId_, chat_id_)); //
-    Logger::getInstance().logInfo(Logger::Levels::Info, ("Message with ID " + std::to_string(messageId_) + " is cursed word!"));
+    ptr_bot_->getApi().sendMessage(chat_id_, message_, nullptr, std::make_shared<TgBot::ReplyParameters>(messageId_, chat_id_));
+    Logger::getInstance().logInfo(Logger::Levels::Info, message_);
 }
 
 EchoReactor::~EchoReactor(){
--- Modules/Server/server.cpp
+++ Modules/Server/server.cpp
@@ -1,45 +1,43 @@
 #include "server.hpp"
 
 
-    Server::Server(std::unique_ptr<TgBot::Bot> ptr_bot, std::shared_ptr<Queue<ITask>> queue): ptr_bot_(std::move(ptr_bot)), queue_(queue){
+Server::Server(std::unique_ptr<TgBot::Bot> ptr_bot, std::shared_ptr<Queue<ITask>> queue): ptr_bot_(std::move(ptr_bot)), queue_(queue){
 
-        ptr_bot_->getEvents().onCommand("start", [&](TgBot::Message::Ptr message) {
-            ptr_bot_->getApi().sendMessage(message->chat->id, "Hi!");
-        });
-       
-        ptr_bot_->getEvents().onAnyMessage([&](TgBot::Message::Ptr message) {
+    ptr_bot_->getEvents().onCommand("start", [&](TgBot::Message::Ptr message) {
+        ptr_bot_->getApi().sendMessage(message->chat->id, "Hi!");
+    });
+    
+    ptr_bot_->getEvents().onAnyMessage([&](TgBot::Message::Ptr message) {
 
-            if (StringTools::startsWith(message->text, "/start")) {
-                return;
+        if (StringTools::startsWith(message->text, "/start")) {
+            return;
+        }
+        if (!queue_->push(std::make_unique<CursedWordDetectingTask>(std::make_shared<SimpleClassificator>(message->text),
+            std::make_shared<EchoReactor>(ptr_bot_, message->text, message->chat->id, message->messageId)))){
+
+            Logger::getInstance().logInfo(Logger::Levels::Critical, "Queue is full!"); 
+            std::this_thread::sleep_for (std::chrono::milliseconds(100));
+        }
+    });
+}
+
+void Server::start(){
+    try {
+            TgBot::TgLongPoll longPoll(*ptr_bot_);
+
+            while (!shutdown_requested) {
+                Logger::getInstance().logInfo(Logger::Levels::Info, "Long poll started");
+                longPoll.start();
             }
-            if (!queue_->push(std::make_unique<CursedWordDetectingTask>(std::make_shared<SimpleClassificator>(message->text),
-             std::make_shared<EchoReactor>(ptr_bot_, message->text, message->chat->id, message->messageId, message->from)))){
+        } catch (TgBot::TgException& e) {
+            Logger::getInstance().logInfo(Logger::Levels::Fatal, e.what());
+        }
+}
 
-                Logger::getInstance().logInfo(Logger::Levels::Critical, "Queue is full!"); 
-                std::this_thread::sleep_for (std::chrono::milliseconds(100));
-            }
-        });
+void Server::terminate(){
+    shutdown_requested = true;
+}
 
+Server::~Server(){
 
-    }
-
-    void Server::start(){
-        try {
-                TgBot::TgLongPoll longPoll(*ptr_bot_);
-
-                while (!shutdown_requested) {
-                    Logger::getInstance().logInfo(Logger::Levels::Info, "Long poll started");
-                    longPoll.start();
-                }
-            } catch (TgBot::TgException& e) {
-                Logger::getInstance().logInfo(Logger::Levels::Fatal, e.what());
-            }
-    }
-
-    void Server::terminate(){
-        shutdown_requested = true;
-    }
-
-    Server::~Server(){
-
-    }
+}
