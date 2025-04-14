316336b40722f2caafca9060bbbbad2fe520f1d8
Jonny251989 <mailbox.lazarev@yandex.ru>
2025-04-14 20:33:08 +0300

    chagnes run server script

--- functional_tests_enviroment.sh
+++ functional_tests_enviroment.sh
@@ -4,7 +4,6 @@
 if [ "$1" == "up" ]; then
   echo "Starting the infrastructure..."
   docker-compose up -d grpc_server
-  docker compose wait grpc_server
 elif [ "$1" == "down" ]; then
   echo "Shutting down the infrastructure..."
   docker-compose  down
