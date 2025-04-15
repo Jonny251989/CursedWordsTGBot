fbe93e1b762f5d40e34b9bf80a711495a49878d5
Jonny251989 <mailbox.lazarev@yandex.ru>
2025-04-15 15:49:38 +0300

    changes

--- docker-compose-run-tests.sh
+++ docker-compose-run-tests.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 
+# Удаление Webhook у Telegram-бота, чтобы избежать конфликта с getUpdates
+echo "Deleting Telegram webhook to avoid getUpdates conflict..."
+curl -s -X POST "https://api.telegram.org/bot${TELEGRAM_BOT_TOKEN}/deleteWebhook"
+
 # Запуск тестов в контейнере test_runner
 docker-compose run --rm test_runner
