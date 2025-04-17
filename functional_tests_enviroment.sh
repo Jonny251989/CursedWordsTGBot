9bb1bb6040fd64b8249d390c04c377312b0a01e0
Jonny251989 <mailbox.lazarev@yandex.ru>
2025-04-17 12:31:07 +0300

    changes CI, scripts and  docker-compose again

--- functional_tests_enviroment.sh
+++ functional_tests_enviroment.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 case "$1" in
-  up)
-    echo "Starting infrastructure..."
-    docker compose up -d grpc_server
-    docker compose wait --timeout 300 grpc_server
-    ;;
-  down)
-    echo "Stopping infrastructure..."
-    docker compose down
-    ;;
-  *)
-    echo "Usage: $0 {up|down}"
-    exit 1
-    ;;
+    up)
+      echo "Starting infrastructure..."
+      docker compose up -d grpc_server
+      ;;
+    down)
+      echo "Stopping infrastructure..."
+      docker compose down
+      ;;
+    *)
+      echo "Usage: $0 {up|down}"
+      exit 1
+      ;;
 esac
