b0234e80df1d8a298598bf9289b061c491daba77
Jonny251989 <mailbox.lazarev@yandex.ru>
2025-04-17 12:22:17 +0300

    changes CI, scripts and  docker-compose again

--- functional_tests_enviroment.sh
+++ functional_tests_enviroment.sh
@@ -1,13 +1,17 @@
 #!/bin/bash
 
-# Проверка параметра
-if [ "$1" == "up" ]; then
-  echo "Starting the infrastructure..."
-  docker-compose up -d grpc_server
-elif [ "$1" == "down" ]; then
-  echo "Shutting down the infrastructure..."
-  docker-compose  down
-else
-  echo "Usage: $0 {up|down}"
-  exit 1
-fi
+case "$1" in
+  up)
+    echo "Starting infrastructure..."
+    docker compose up -d grpc_server
+    docker compose wait --timeout 300 grpc_server
+    ;;
+  down)
+    echo "Stopping infrastructure..."
+    docker compose down
+    ;;
+  *)
+    echo "Usage: $0 {up|down}"
+    exit 1
+    ;;
+esac
