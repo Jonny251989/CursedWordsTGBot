13bd4e511aedaaa7c5902e360f34c6f156836263
Jonny251989 <mailbox.lazarev@yandex.ru>
2025-04-17 12:42:16 +0300

    changes CI, scripts and  docker-compose again

--- docker-compose-run-tests.sh
+++ docker-compose-run-tests.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Ждём готовности grpc_server (максимум 5 минут)
-docker compose wait --timeout 300 --condition=healthy grpc_server
 
 # Запускаем тесты
 docker compose run --rm test_runner
